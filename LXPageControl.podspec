92466251730de61a6d059754043ffd0623140e04
Fatih Ozkul <fatih@fatih-ozkul.com>
2023-01-19 09:15:52 -0500

    Add package support

--- LXPageControl.podspec
+++ LXPageControl.podspec
@@ -8,5 +8,5 @@ Pod::Spec.new do |s|
   s.source           = { :git => 'https://github.com/leonx98/LXPageControl.git', :tag => s.version }
   s.swift_version = '4.0'
   s.ios.deployment_target = '9.0'
-  s.source_files = 'LXPageControl/Classes/**/*'
+  s.source_files = 'Sources/LXPageControl/Classes/**/*'
 end
---
+++ Tests/LXPageControlTests/LXPageControlTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LXPageControl
+
+final class LXPageControlTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LXPageControl().text, "Hello, World!")
+    }
+}
